1e3d76b62889e264592f3b35916fce8ff1f7cfd3
Bhagwat Sagute <73931101+bsagute@users.noreply.github.com>
2024-07-22 15:38:50 +0530

    Update 1a.go

--- 1a.go
+++ 1a.go
@@ -1,12 +1,9 @@
-package api_test
+package api
 
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"github.com/your_module_path/api"
-	"github.com/your_module_path/entity"
-	"github.com/your_module_path/model"
 )
 
 // MockApplistRepository is a mock of the ApplistRepository interface
@@ -14,46 +11,46 @@ type MockApplistRepository struct {
 	mock.Mock
 }
 
-func (m *MockApplistRepository) FindAllComp() (model.Applist, error) {
+func (m *MockApplistRepository) FindAllComp() (Applist, error) {
 	args := m.Called()
-	return args.Get(0).(model.Applist), args.Error(1)
+	return args.Get(0).(Applist), args.Error(1)
 }
 
-func (m *MockApplistRepository) FindServiceMapData(serviceId string) ([]model.ServiceInfoRes, error) {
+func (m *MockApplistRepository) FindServiceMapData(serviceId string) ([]ServiceInfoRes, error) {
 	args := m.Called(serviceId)
-	return args.Get(0).([]model.ServiceInfoRes), args.Error(1)
+	return args.Get(0).([]ServiceInfoRes), args.Error(1)
 }
 
-func (m *MockApplistRepository) FindMetricsMetadata(requestedEntityId string, entityTypes []string) (model.MetricMetadataResponse, error) {
+func (m *MockApplistRepository) FindMetricsMetadata(requestedEntityId string, entityTypes []string) ([]MetricMetadataResponse, error) {
 	args := m.Called(requestedEntityId, entityTypes)
-	return args.Get(0).(model.MetricMetadataResponse), args.Error(1)
+	return args.Get(0).([]MetricMetadataResponse), args.Error(1)
 }
 
-func (m *MockApplistRepository) CheckServiceMapByServiceId(serviceId string) (*model.ServiceInfo, error) {
+func (m *MockApplistRepository) CheckServiceMapByServiceId(serviceId string) (*ServiceInfo, error) {
 	args := m.Called(serviceId)
-	return args.Get(0).(*model.ServiceInfo), args.Error(1)
+	return args.Get(0).(*ServiceInfo), args.Error(1)
 }
 
-func (m *MockApplistRepository) FindJourneyDetails(params entity.JourneyParams) ([]entity.JourneyItsmDetailsRes, error) {
+func (m *MockApplistRepository) FindJourneyDetails(params JourneyParams) ([]JourneyItsmDetailsRes, error) {
 	args := m.Called(params)
-	return args.Get(0).([]entity.JourneyItsmDetailsRes), args.Error(1)
+	return args.Get(0).([]JourneyItsmDetailsRes), args.Error(1)
 }
 
-func (m *MockApplistRepository) FindJourneyList() ([]entity.JourneyListRes, error) {
+func (m *MockApplistRepository) FindJourneyList() ([]JourneyListRes, error) {
 	args := m.Called()
-	return args.Get(0).([]entity.JourneyListRes), args.Error(1)
+	return args.Get(0).([]JourneyListRes), args.Error(1)
 }
 
 // NewApplistApi creates a new instance of DefaultApplistApi
-func NewApplistApi(repository model.ApplistRepository) *api.DefaultApplistApi {
-	return &api.DefaultApplistApi{Repo: repository}
+func NewApplistApi(repository ApplistRepository) *DefaultApplistApi {
+	return &DefaultApplistApi{repo: repository}
 }
 
 // Test functions
 
 func TestGetAllAppComp(t *testing.T) {
 	mockRepo := new(MockApplistRepository)
-	expectedResult := model.Applist{ /* initialize with some data */ }
+	expectedResult := Applist{ /* initialize with some data */ }
 	mockRepo.On("FindAllComp").Return(expectedResult, nil)
 
 	api := NewApplistApi(mockRepo)
@@ -67,7 +64,7 @@ func TestGetAllAppComp(t *testing.T) {
 func TestGetServiceMapData(t *testing.T) {
 	mockRepo := new(MockApplistRepository)
 	serviceId := "test-service-id"
-	expectedResult := []model.ServiceInfoRes{ /* initialize with some data */ }
+	expectedResult := []ServiceInfoRes{ /* initialize with some data */ }
 	mockRepo.On("FindServiceMapData", serviceId).Return(expectedResult, nil)
 
 	api := NewApplistApi(mockRepo)
@@ -82,7 +79,7 @@ func TestGetMetricsMetadata(t *testing.T) {
 	mockRepo := new(MockApplistRepository)
 	requestedEntityId := "test-entity-id"
 	entityTypes := []string{"type1", "type2"}
-	expectedResult := model.MetricMetadataResponse{ /* initialize with some data */ }
+	expectedResult := []MetricMetadataResponse{ /* initialize with some data */ }
 	mockRepo.On("FindMetricsMetadata", requestedEntityId, entityTypes).Return(expectedResult, nil)
 
 	api := NewApplistApi(mockRepo)
@@ -96,7 +93,7 @@ func TestGetMetricsMetadata(t *testing.T) {
 func TestCheckServiceMap(t *testing.T) {
 	mockRepo := new(MockApplistRepository)
 	serviceId := "test-service-id"
-	expectedResult := &model.ServiceInfo{ /* initialize with some data */ }
+	expectedResult := &ServiceInfo{ /* initialize with some data */ }
 	mockRepo.On("CheckServiceMapByServiceId", serviceId).Return(expectedResult, nil)
 
 	api := NewApplistApi(mockRepo)
@@ -109,8 +106,8 @@ func TestCheckServiceMap(t *testing.T) {
 
 func TestGetJourneyDetails(t *testing.T) {
 	mockRepo := new(MockApplistRepository)
-	params := entity.JourneyParams{ /* initialize with some data */ }
-	expectedResult := []entity.JourneyItsmDetailsRes{ /* initialize with some data */ }
+	params := JourneyParams{ /* initialize with some data */ }
+	expectedResult := []JourneyItsmDetailsRes{ /* initialize with some data */ }
 	mockRepo.On("FindJourneyDetails", params).Return(expectedResult, nil)
 
 	api := NewApplistApi(mockRepo)
@@ -123,7 +120,7 @@ func TestGetJourneyDetails(t *testing.T) {
 
 func TestGetJourneyList(t *testing.T) {
 	mockRepo := new(MockApplistRepository)
-	expectedResult := []entity.JourneyListRes{ /* initialize with some data */ }
+	expectedResult := []JourneyListRes{ /* initialize with some data */ }
 	mockRepo.On("FindJourneyList").Return(expectedResult, nil)
 
 	api := NewApplistApi(mockRepo)
